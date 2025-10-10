09306b9b55e2d369562b9a71bdf80f675dcf1064
Nugget1368 <rneg1@hotmail.com>
2025-10-10 11:03:30 +0200

    react-routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import './App.css'
 import BookingForms from './components/forms/BookingForms'
+import { BrowserRouter as Router, Route, Routes } from 'react-router'
 
 function App() {
   
 
   return (
-    <>
-      <section>
-        <article>
-          <BookingForms />
-        </article>
-      </section>
-      
-    </>
+      <Router>
+        <Routes>
+          <Route path="/" element={<BookingForms />} />
+          <Route path="*" element={<h1>404 Not found friend...</h1>} />
+        </Routes>
+      </Router>
   )
 }
 
