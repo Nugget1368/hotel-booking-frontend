685c151cc6a406ae7a1f2929052d21d822918b8d
Nugget1368 <rneg1@hotmail.com>
2025-10-14 11:03:05 +0200

    Created Login-page

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import BookingForms from './components/forms/BookingForms'
 import HotelsPage from './pages/HotelsPage'
 import Header from './components/Header'
 import { BrowserRouter as Router, Route, Routes } from 'react-router'
+import LoginPage from './pages/LoginPage'
 
 function App() {
-
-
   return (
       <Router>
         <Routes>
@@ -16,6 +15,7 @@ function App() {
           </> 
           } />
           <Route path="/booking" element={<BookingForms />} />
+          <Route path="/profile/login" element={<LoginPage />} />
           <Route path="*" element={<h1>404 Not found friend...</h1>} />
         </Routes>
       </Router>
