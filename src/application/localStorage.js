1151cc34607327bbe6cc5c8124b11b1a77028247
Nugget1368 <rneg1@hotmail.com>
2025-10-20 13:29:45 +0200

    super basic logout Functionality

--- src/application/localStorage.js
+++ src/application/localStorage.js
@@ -1,5 +1,5 @@
 const USER_TOKEN_KEY = "user_token";
-const USER_KEY = "user";
+const USER_INFO_KEY = "user";
 export class MySessionStorage {
     static getUserToken() {
         return sessionStorage.getItem(USER_TOKEN_KEY) ? JSON.parse(sessionStorage.getItem(USER_TOKEN_KEY)) : null;
@@ -17,10 +17,25 @@ export class MySessionStorage {
             email: user.email,
             phone: user.phone
         }
-        sessionStorage.setItem(USER_KEY, JSON.stringify(userInfo));
+        sessionStorage.setItem(USER_INFO_KEY, JSON.stringify(userInfo));
     }
 
     static getUserInfo() {
-        return sessionStorage.getItem(USER_KEY) ? JSON.parse(sessionStorage.getItem(USER_KEY)) : null;
+        return sessionStorage.getItem(USER_INFO_KEY) ? JSON.parse(sessionStorage.getItem(USER_INFO_KEY)) : null;
+    }
+
+    static removeUserInfo() {
+        if (sessionStorage.getItem(USER_INFO_KEY))
+            sessionStorage.removeItem(USER_INFO_KEY);
+    }
+
+    static removeUserToken() {
+        if (sessionStorage.getItem(USER_TOKEN_KEY))
+            sessionStorage.removeItem(USER_TOKEN_KEY);
+    }
+
+    static removeUser() {
+        this.removeUserInfo();
+        this.removeUserToken();
     }
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router"
+import { useState, useEffect } from "react";
+import {MySessionStorage} from "../application/localStorage"
 
 const Header = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    MySessionStorage.getUserInfo() ? setLoggedIn(true) : setLoggedIn(false);
+  }, [loggedIn]);
+
+  const logout = () => {
+    MySessionStorage.removeUser();
+    setLoggedIn(false);
+  }
+
   return (
     <header id="main-header">
         <div className="row"></div>
         <div className="row">
+            { loggedIn ?
+            <button onClick={logout}>Log out</button> :
             <Link to="/profile/login">Login</Link>
+            }
         </div>
     </header>
   )
